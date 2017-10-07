4eb7d620353770487a549d11c5b44fb7d0218906
Simone Scalabrino <s.scalabrino9@gmail.com>
2017-10-07 21:55:22 +0200

    Mega refactoring

---
+++ backend/src/test/java/it/unimol/anpr_github_metrics/github/AuthenticatorTest.java
@@ -0,0 +1,21 @@
+package it.unimol.anpr_github_metrics.github;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Simone Scalabrino.
+ */
+public class AuthenticatorTest {
+
+    @Test
+    public void test() {
+        String token = "f86035f8bc5c425839f9b4fec06fcc441e8b8d30";
+        assertNotNull(token);
+
+//        Authenticator.getInstance().authenticate(token);
+
+//        assertNotNull(Authenticator.getInstance().getGitHub());
+    }
+}
