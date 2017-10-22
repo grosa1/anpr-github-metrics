333b4c6cd3d850670fbe7877642cdd3b73627790
giovanni.rosa <g.rosa1@studenti.unimol.it>
2017-10-22 19:19:47 +0200

    fixed login. We have to set login params in a config file

--- backend/src/main/java/it/unimol/anpr_github_metrics/services/LoginApi.java
+++ backend/src/main/java/it/unimol/anpr_github_metrics/services/LoginApi.java
@@ -8,49 +8,64 @@ import it.unimol.anpr_github_metrics.github.Authenticator;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.validation.constraints.Null;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
-
-@Path("/github")
+@Path("/login")
 public class LoginApi {
+
+    public static final String ON_LOGIN_REDIRECT_URI = "http://www.unimol.it";
+
     @GET
     @Path("/getLoginCode")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getToken(@QueryParam("code") String code, @QueryParam("state") String state, @Context HttpServletRequest request) {
+
         String token;
+        HttpResponse<String> res = null;
+        try {
+            res = Unirest.post("https://github.com/login/oauth/access_token")
+                    .field("client_id", "1211d954012cf73c2e2b")
+                    .field("client_secret", "1237664d8ab78f6305d2571ee7189fdc5b641ef6")
+                    .field("code", code)
+                    .field("redirect_uri", ON_LOGIN_REDIRECT_URI)
+                    .field("state", state)
+                    .asString();
 
-//        try {
-//            HttpResponse<String> tokenRes = Unirest.post("https://github.com/login/oauth/access_token")
-//                    .field("client_id", "1211d954012cf73c2e2b")
-//                    .field("client_secret", "1237664d8ab78f6305d2571ee7189fdc5b641ef6")
-//                    .field("code", code)
-//                    .field("redirect_uri", "http://www.unimol.it")
-//                    .field("state", state)
-//                    .asString();
-//
-//            Map<String, String> map = getQueryMap(tokenRes.getBody());
-//
-//            token = map.get("access_token");
-//
-//            System.out.println(token);
-//
-//            //TODO check
-//            HttpSession session = request.getSession();
-//
-//            Github github = Authenticator.getInstance().authenticate(token).getGitHub();
-//            session.setAttribute("token", token);
-//            session.setAttribute("github", github);
-            
-            return Response.status(Response.Status.OK).entity("prova").build();
+            if(res.getBody().contains("error")) {
+                throw new ForbiddenException();
+            }
 
-//        } catch (UnirestException e) {
-//            e.printStackTrace();
-//            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-//        }
+            Map<String, String> map = getQueryMap(res.getBody());
+            token = map.get("access_token");
+
+            //TODO check
+            HttpSession session = request.getSession();
+
+            Github github = Authenticator.getInstance().authenticate(token).getGitHub();
+            session.setAttribute("token", token);
+            session.setAttribute("github", github);
+
+            URI uri = new URI(ON_LOGIN_REDIRECT_URI);
+            return Response.seeOther(uri).status(Response.Status.OK).build();
+
+        } catch (UnirestException e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        } catch (ForbiddenException e) {
+            System.out.println(res.getBody().toString());
+            return Response.status(Response.Status.FORBIDDEN).build();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+
+        }
     }
 
     public Map<String, String> getQueryMap(String query) {
